21ed2e6fb7fd193829ce30e0aeb8d9d965499a36
Asma Ghandeharioun <asma@dhcp-18-30-80-208.dyn.mit.edu>
2019-02-08 01:30:37 -0500

    reverting format

--- create_email.py
+++ create_email.py
@@ -6,17 +6,17 @@ Template:
 [Ashdown] Ashdown-Anno for [date]
 
 Summary
-1. [title], [date], [time]
-2. [title], [date], [time]
+1. [title], [date], [time]{-[time]}
+2. [title], [date], [time]{-[time]}
 ...
 ------------------------
 1. [title]
-[date], [time], [location]
+[date], [time]{-[time]}, [location]
 [event detail]
 [poster image if available]
 -------------------------
 2. [title]
-[date], [time], [location]
+[date], [time]{-[time]}, [location]
 [event detail]
 [poster image if available]
 ...
@@ -42,26 +42,31 @@ def create_human_readable_date(inp_date):
 def create_human_readable_time(inp_time):
     return dt.datetime.strptime(inp_time, Utils.TIME_FORMAT).strftime(Utils.HUMAN_READABLE_TIME_FORMAT)
 
+def create_human_readable_end_time(inp_time):
+    if inp_time == '':
+        return ''
+    return ' - ' + dt.datetime.strptime(inp_time, Utils.TIME_FORMAT).strftime(Utils.HUMAN_READABLE_TIME_FORMAT)
 
 def create_summary_item(idx, row):
     txt = str(idx) + '. ' + row['title'] + ', ' + create_human_readable_date(row['date']) + ', ' + \
-          create_human_readable_time(row['time']) + '\n'
+          create_human_readable_time(row['start_time']) + create_human_readable_end_time(row['end_time']) + '\n'
 
     html = '<b>' + str(idx) + '. ' + row['title'] + ', ' + create_human_readable_date(row['date']) + ', ' + \
-             create_human_readable_time(row['time']) + '</b> <br>'
+             create_human_readable_time(row['start_time']) + create_human_readable_end_time(row['end_time']) + \
+           '</b> <br>'
     return txt, html
 
 
 def create_full_item(idx, row):
     txt = '\n' + \
              str(idx) + '. ' + row['title'] + '\n' + create_human_readable_date(row['date']) + ', ' + \
-             create_human_readable_time(row['time']) + ', ' + row['location'] + '\n' + \
-             row['description'] + '\n'
+          create_human_readable_time(row['start_time']) + create_human_readable_end_time(row['end_time']) + ', ' + \
+          row['location'] + '\n' + row['description'] + '\n'
 
-    html = '<hr><br><div style="padding-left:15px;">' + \
+    html = '<hr>' + \
            '<b>' + str(idx) + '. ' + row['title'] + '<br>' + create_human_readable_date(row['date']) + ', ' + \
-           create_human_readable_time(row['time']) + ', ' + row['location'] + '</b> <br>' + \
-           row['description'] + '<br></div>'
+           create_human_readable_time(row['start_time']) + create_human_readable_end_time(row['end_time']) + ', ' + \
+           row['location'] + '</b> <br>' + row['description'] + '<br>'
     if row['image']:
         ext = row['image'][-4:].lower()
         if ext == '.pdf':
@@ -80,7 +85,7 @@ def create_full_item(idx, row):
     return txt, html
 
 
-def create_email(from_email, curr_date, to_email):
+def create_email(from_email, from_pass, curr_date, to_email):
 
     event_df = parse_events(curr_date)
 
@@ -90,8 +95,8 @@ def create_email(from_email, curr_date, to_email):
 
     # Create message container - the correct MIME type is multipart/alternative.
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = '[Ashdown] Ashdown-Anno for ' + create_human_readable_date(curr_date)
-    msg['From'] = 'Ashdown Housing Events <' + from_email + '>'
+    msg['Subject'] = 'Ashdown-Anno for ' + create_human_readable_date(curr_date)
+    msg['From'] = 'Ashdown Announcements <' + from_email + '>'
     msg['To'] = to_email
 
     # Create the body of the message (a plain-text and an HTML version).
@@ -110,44 +115,15 @@ def create_email(from_email, curr_date, to_email):
         </style>
       </head>
       <body>
-        <table width="900" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff" style="border-left:3px solid black;border-right:3px solid black;border-bottom:3px solid black;">
-        <!-- begin logo row -->
-        <tr>
-        <!-- begin ashdown logo cell -->
-        <td valign="middle" bgcolor="#333333" colspan="2"><a href="http://ashdown.mit.edu/"><img src="https://ashdown.mit.edu/images/toplogo3.png" width="900" height="60" border="0" alt="Ashdown Logo"/></a></td>
-        <!-- end ashdown logo cell -->
-        </tr>
-        <!-- end logo row -->
-        <!-- begin horizontal line effect -->
-        <tr><td width="900" colspan="2"><img src="https://ashdown.mit.edu/images/gradline.gif" width="900" height="18" border="0" alt="Line"></td></tr>
-        <!-- end horizontal line effect -->
-        <!-- begin main part of page -->
-        <tr>
-        <style type="text/css">
-        <!--
-        .blogtitle {font-family:Georgia,UnBatang,DejaVu Serif,Verdana,Trebuchet MS,FreeSerif,Arial,Helvetica,serif;font-size:32px;color:#000000;line-height:1.2em;}
-        .blogsubtitle {font-family:Georgia,UnBatang,DejaVu Serif,Verdana,Trebuchet MS,FreeSerif,Arial,Helvetica,serif;font-size:12px;color:#000000;line-height:1em;}
-        .blogdescription {font-family:Droid Sans,Trebuchet MS,Verdana,FreeSerif,Arial,Helvetica,serif;font-size:12px;color:#000000;line-height:2em;}
-        .blogsponsors {font-family:Droid Sans,Trebuchet MS,Verdana,FreeSerif,Arial,Helvetica,serif;font-size:10px;color:#808080;line-height:2em;margin-top:10px;}
-        -->
-        </style>
-        <div style="border-left:10px solid #eecea4;padding-left:15px;"><h1>
-        Ashdown Events</h1><div class="blogsubtitle">Week of """ + create_human_readable_date(curr_date) + """
-        </div></div><br/>
-        <table border="0" cellspacing="0" cellpadding="0"><tr><td valign="top">
-        <br><br>
-        <div style="border-left:10px solid #e0e0e0;padding-left:15px;">
-        <h1>Summary</h1></div>
-        <div style="padding-left:15px;">
+        <h1>Ashdown Events</h1>
+        <h2>Summary</h2>
+        <br>
         """
 
     for idx, row in event_df.iterrows():
         row_txt, row_html = create_summary_item(idx+1, row)
         txt += row_txt
         html += row_html
-    html += """
-        </div>
-    """
     for idx, row in event_df.iterrows():
         row_txt, row_html = create_full_item(idx+1, row)
         txt += row_txt
@@ -157,7 +133,7 @@ def create_email(from_email, curr_date, to_email):
         </table><br/>
         <div align="center" class="smalltext" style="color:#A0A0A0;">
         &copy; 2019 MIT.
-        Please report feedback about this website to ashdown-webmaster (at) mit.edu.<br/><br/>
+        Please report feedback about this website to ashdown-tech (at) mit.edu.<br/><br/>
         </div>
       </body>
     </html>
@@ -175,7 +151,12 @@ def create_email(from_email, curr_date, to_email):
 
     # Send the message via local SMTP server.
 
-    server = smtplib.SMTP('outgoing.mit.edu:25')  # 'smtp.gmail.com:587' NOTE:  This is the GMAIL SSL port.
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    # gmail SSL port: 'smtp.gmail.com:587'
+    # MIT port: outgoing.mit.edu:25
+
+    server.starttls()
+    server.login(from_email, from_pass)
 
     # sendmail function takes 3 arguments: sender's address, recipient's address
     # and message to send - here it is sent as one string.
