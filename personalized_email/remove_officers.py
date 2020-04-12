785a963505da59cb4b342668781cd5fb663211d9
Asma Ghandeharioun <asma_gh@mit.edu>
2020-04-12 13:54:13 -0400

    removing anyone else that needs to be excluded

--- personalized_email/remove_officers.py
+++ personalized_email/remove_officers.py
@@ -5,7 +5,10 @@ import os
 if __name__ == "__main__":
     all_residents = pd.read_csv(os.path.join('data', 'roster_retrieved_emails_postprocessed.csv'), index_col=0)
     officers = pd.read_csv(os.path.join('data', 'officers.txt'), header=None)
-    officers.columns = ['kerberos']
-    output_df = all_residents.loc[pd.merge(all_residents, officers, on=['kerberos'], how='left', indicator=True)['_merge'] == 'left_only']
-    output_df.to_csv(os.path.join('data', 'all_but_officers.csv'))
+    exclude = pd.read_csv(os.path.join('data', 'exclude.txt'), header=None)
+    exclude = pd.concat([officers, exclude])
+    exclude.columns = ['kerberos']
+
+    output_df = all_residents.loc[pd.merge(all_residents, exclude, on=['kerberos'], how='left', indicator=True)['_merge'] == 'left_only']
+    output_df.to_csv(os.path.join('data', 'all_but_officers_and_exclusions.csv'))
     print(output_df)
